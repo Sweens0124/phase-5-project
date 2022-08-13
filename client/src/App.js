81be1d975e26787cd901bcb8cc564c0a8376d12a
Lucas Sweeney <lsweeney0124@gmail.com>
2022-08-12 22:59:26 -0600

    added signup page, added routes needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import SignIn from './SignIn';
+import Signup from './Signup'
 
 
 function App () {
@@ -8,6 +9,7 @@ function App () {
     <Routes>
       <Route path="/" element={ <Home /> } />
       <Route path="/sign-in" element={ <SignIn /> } />
+      <Route path="/signup" element={ <Signup /> } />
     </Routes>
   );
 }
