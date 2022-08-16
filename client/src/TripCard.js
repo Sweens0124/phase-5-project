e1c086da1d674b18565c8b13ad496391c027af0d
Lucas Sweeney <lsweeney0124@gmail.com>
2022-08-15 21:28:31 -0600

    styling trip cards

--- client/src/TripCard.js
+++ client/src/TripCard.js
@@ -1,21 +1,24 @@
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 const TripCard = ({ trip }) => {
   const { location, date, single_price, double_price, hotel, hotel_desc, image } = trip
 
   return (
     <Grid item xs={ 12 }>
-      <Paper elevation={ 3 }>
-        <img
-          src={ image }
-          alt="Image of hotel"
-          className="img"
-        />
-        <Typography component="h5" variant="h5">
-          { location }
-        </Typography>
+      <Paper elevation={ 15 }>
+        <Box padding={ 1 } sx={ { width: 425 } }>
+          <Typography component="h5" variant="h5">
+            { location }
+          </Typography>
+          <img
+            src={ image }
+            alt="Image of hotel"
+            className="img"
+          />
+        </Box>
       </Paper>
     </Grid>
 
