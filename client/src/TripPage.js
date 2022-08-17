c45e682e292a524a96bba3d0cb1129569bb9d34a
Lucas Sweeney <lsweeney0124@gmail.com>
2022-08-17 15:52:31 -0600

    added image collage to trip page

--- client/src/TripPage.js
+++ client/src/TripPage.js
@@ -2,6 +2,8 @@ import { Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import Navbar from './Navbar';
 import { useParams } from 'react-router-dom';
+import Box from '@mui/material/Box'
+import ImageCollage from './ImageCollage'
 
 
 const TripPage = ({ trips }) => {
@@ -20,6 +22,11 @@ const TripPage = ({ trips }) => {
         <Typography variant="h3" component="h1" marginTop={ 3 } >
           Visit { trip?.location }!
         </Typography>
+        <Box marginTop={ 3 } sx={ { display: 'flex' } }>
+          <img src='https://media.istockphoto.com/photos/villefranchesurmer-village-in-france-picture-id1248448159?k=20&m=1248448159&s=612x612&w=0&h=leahrG95LcBDfdkPCavNL9W8ZC2OroNZPqO-196HDPU=' alt='' height={ 325 } />
+          <ImageCollage />
+        </Box>
+
       </Container>
     </>
   )
